f81bb6459c2830c29b443c654a516b7784523a8c
Akhil <akhil.gs@gmail.com>
2020-04-27 21:26:52 -0400

    Updated docs

--- lib/src/wit_ai_client.dart
+++ lib/src/wit_ai_client.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'network.dart';
 
+/// This class is a simple client for Wit.ai
+/// 
+/// It has a single method at the current moment to message your Wit app.
+/// Other API methods coming soon!
+/// 
+/// @author Akhil Raj
+/// @version 0.0.1
+/// 
+/// GitHub - "https://github.com/akhil-raj-git/wit_ai"
 class Wit {
-  Wit({this.accessToken});
+  
+  Wit({@required this.accessToken});
 
+  /// Access token must be passed.
+  /// You can find it in your Wit app's settings.
+  /// It should be titled "Client Access Token"
   final String accessToken;
 
+  /// The message method takes a String parameter 'message' to
+  /// send to your Wit app. It returns a dynamic object.
   Future message(String message) async {
     String query = Uri.encodeComponent(message);
     String uri = 'https://api.wit.ai/message?q=' + query;
