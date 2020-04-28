fa31de3a1600575b774668124e76b3905e8d5e94
Akhil <akhil.gs@gmail.com>
2020-04-27 22:58:09 -0400

    CHANGELOG update

--- test/wit_ai_test.dart
+++ test/wit_ai_test.dart
@@ -3,11 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wit_ai/wit_ai.dart';
 
 void main() {
+  final wit = 
+        Wit(accessToken: "6EQHTQPEZPQ6NJ45EHAIPLHEZU6QCTD4");
   test("Checks if the method message returns the correct value", 
     () async {
-      final wit = 
-        Wit(accessToken: "ACCESS_TOKEN_HERE");
-      
       String query = "Hello";
       dynamic response = await wit.message(query);
       print(response);
